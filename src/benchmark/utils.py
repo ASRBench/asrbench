eb7115dc4ab861d6275b236b20459f006a48ee1a
BDu4rTe <brianschierduarte@outlook.com>
2024-09-20 15:33:05 -0300

    feat(benchmark/utils.py): add functions to calc rtf(Real Time Factor) [get_rtf, __get_audio_duration].

--- src/benchmark/utils.py
+++ src/benchmark/utils.py
@@ -1,3 +1,4 @@
+from pydub import AudioSegment
 from pathlib import Path
 
 
@@ -10,3 +11,14 @@ def check_path(filepath: str) -> None:
 
     if not path.exists():
         raise FileNotFoundError("File does not exists.")
+
+
+def get_rtf(runtime: float, audio_path: str) -> float:
+    rtf: float = runtime / __get_audio_duration(audio_path)
+    return round(rtf, 2)
+
+
+def __get_audio_duration(audio_path: str) -> float:
+    audio = AudioSegment.from_wav(audio_path)
+    if audio is not None:
+        return len(audio) / 1000
