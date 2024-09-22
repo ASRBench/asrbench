89506edd00862361b67749335aeee1051afbbf7c
BDu4rTe <brianschierduarte@outlook.com>
2024-09-21 23:27:19 -0300

    feat(providers): add property params in IaProvider

--- benchmark/providers/abc_provider.py
+++ benchmark/providers/abc_provider.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Any
 
 
 class IaProvider(ABC):
+
+    @property
+    @abstractmethod
+    def params(self) -> Dict[str, Any]:
+        raise NotImplementedError("Implement params property.")
 
     @abstractmethod
     def transcribe(self, audio_path: str) -> str:
--- benchmark/providers/faster_whisper_.py
+++ benchmark/providers/faster_whisper_.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Dict, Any
 
 from faster_whisper import WhisperModel
 
@@ -15,12 +16,17 @@ class FasterWhisperCfg:
 
 class FasterWhisper(IaProvider):
     def __init__(self, cfg: FasterWhisperCfg):
+        self.__params: Dict[str, Any] = cfg.__dict__
         self.__beam_size: int = cfg.beam_size
         self.__model = WhisperModel(
             model_size_or_path=cfg.model_size,
             compute_type=cfg.compute_type,
             device=cfg.device,
         )
+
+    @property
+    def params(self) -> Dict[str, Any]:
+        return self.__params
 
     @property
     def beam_size(self) -> int:
