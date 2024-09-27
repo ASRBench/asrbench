55a69db5866fa6709b3a070da29f36d008ed7a21
BDu4rTe <brianschierduarte@outlook.com>
2024-09-27 15:01:36 -0300

    feat(benchmark/dataset_benchmark.py): add list of Dataset support

--- benchmark/dataset_benchmark.py
+++ benchmark/dataset_benchmark.py
@@ -4,47 +4,58 @@ from .abc_benchmark import BenchmarkABC
 from .dataset import Dataset
 from .dtos.common import TranscribeResult
 from .providers.abc_provider import IaProvider
+from typing import List
 
 
 class DatasetBenchmark(BenchmarkABC):
-    def __init__(self, dataset: Dataset) -> None:
+    def __init__(self, datasets: List[Dataset],
+                 name: str = "benchmark") -> None:
         super().__init__()
-        self.__dataset: Dataset = dataset
+        self.__datasets: List[Dataset] = datasets
+        self.__name: str = name
 
     def run(self) -> None:
-        with open(
-                self._get_output_filename(self.__dataset.name),
-                "w",
-        ) as csv_file:
+        output_filename: str = self._get_output_filename(self.__name)
+
+        with open(output_filename, "w") as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=self._get_fieldnames())
             writer.writeheader()
 
-            for provider_name, provider in self.providers.items():
-                for pair in self.__dataset.pairs:
+        for dataset in self.__datasets:
+            self._process_dataset_with_all_providers(dataset, writer)
+
+    def run_with_provider(self, name: str) -> None:
+        provider: IaProvider = self._get_provider(name)
+        output_filename: str = self._get_output_filename(
+            f"{self.__name}_{provider.__class__.__name__}",
+        )
+
+        with open(output_filename, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self._get_fieldnames())
+            writer.writeheader()
+
+            for dataset in self.__datasets:
+                for pair in dataset.pairs:
                     result: TranscribeResult = self._run_provider(
                         provider,
                         pair.audio,
-                        pair.reference
+                        pair.reference,
                     )
 
                     pprint(result.__dict__)
                     writer.writerow(result.__dict__)
 
-    def run_with_provider(self, name: str) -> None:
-        provider: IaProvider = self._get_provider(name)
-
-        with open(
-                self._get_output_filename(f"{self.__dataset.name}_{name}"),
-                "w",
-        ) as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=self._get_fieldnames())
-            writer.writeheader()
-
-            for pair in self.__dataset.pairs:
+    def _process_dataset_with_all_providers(
+            self,
+            dataset: Dataset,
+            writer: csv.DictWriter[str],
+    ) -> None:
+        for provider_name, provider in self.providers:
+            for pair in dataset.pairs:
                 result: TranscribeResult = self._run_provider(
                     provider,
                     pair.audio,
-                    pair.reference,
+                    pair.reference
                 )
 
                 pprint(result.__dict__)
