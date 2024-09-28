e5d31213623b5fec8720be9855e6444edde74092
BDu4rTe <brianschierduarte@outlook.com>
2024-09-28 20:58:23 -0300

    feat: add log config in main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import logging
 from wavbench.config_loader import BenchmarkFactory
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format=(
+        "%(asctime)s : %(levelname)s :  %(message)s -  %(name)s:%(lineno)d"
+    ),
+    filename="wavbench.log",
+    filemode="a",
+)
 
 benchmark = BenchmarkFactory.from_config("dataset_benchmark_exemple.yml")
 benchmark.run()
