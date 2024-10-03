44271032b34f32ef9a89d64c24f34d10f5c6cb20
BDu4rTe <brianschierduarte@outlook.com>
2024-10-03 20:01:03 -0300

    feat(wavbench/measures): add functions for remove accents and get all measures provided by jiwer

--- wavbench/measures.py
+++ wavbench/measures.py
@@ -1,4 +1,14 @@
 import jiwer
+import unicodedata
+from .dtos.common import Measures
+
+
+def _remove_accents(text: str) -> str:
+    return ''.join(
+        char for char in unicodedata.normalize('NFD', text)
+        if unicodedata.category(char) != 'Mn'
+    )
+
 
 transform = jiwer.Compose(
     [
@@ -7,12 +17,25 @@ transform = jiwer.Compose(
         jiwer.RemoveMultipleSpaces(),
         jiwer.Strip(),
         jiwer.RemovePunctuation(),
+        _remove_accents,
         jiwer.ReduceToListOfListOfWords(),
     ]
 )
 
 
+def get_measures(reference: str, hypothesis: str) -> Measures:
+    """Returns all measures provided by jiwer (wer, cer, mer, wil, wip)"""
+    return Measures(
+        wer=get_wer(reference, hypothesis),
+        cer=get_cer(reference, hypothesis),
+        mer=get_mer(reference, hypothesis),
+        wil=get_wil(reference, hypothesis),
+        wip=get_wip(reference, hypothesis)
+    )
+
+
 def get_wer(reference: str, hypothesis: str) -> float:
+    """Measure Word Error Rate [WER]"""
     return round(
         jiwer.wer(
             reference=reference,
@@ -25,6 +48,7 @@ def get_wer(reference: str, hypothesis: str) -> float:
 
 
 def get_cer(reference: str, hypothesis: str) -> float:
+    """Measure Character Error Rate [CER]"""
     return round(
         jiwer.cer(
             reference=reference,
@@ -37,6 +61,7 @@ def get_cer(reference: str, hypothesis: str) -> float:
 
 
 def get_mer(reference: str, hypothesis: str) -> float:
+    """Measure Match Error Rate [MER]"""
     return round(
         jiwer.mer(
             reference=reference,
@@ -49,6 +74,7 @@ def get_mer(reference: str, hypothesis: str) -> float:
 
 
 def get_wil(reference: str, hypothesis: str) -> float:
+    """Measure Word Information Lost [WIL]"""
     return round(
         jiwer.wil(
             reference=reference,
@@ -61,6 +87,7 @@ def get_wil(reference: str, hypothesis: str) -> float:
 
 
 def get_wip(reference: str, hypothesis: str) -> float:
+    """Measure Word Information Preserved [WIP]"""
     return round(
         jiwer.wip(
             reference=reference,
