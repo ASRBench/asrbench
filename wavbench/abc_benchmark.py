78aac4423da1528f3d8db2710f8054fa6a873c58
BDu4rTe <brianschierduarte@outlook.com>
2024-10-29 20:44:41 -0300

    feat(wavbench/abc_benchmark): remove get_output_filename, get_fieldnames, get_provider and run_provider

--- wavbench/abc_benchmark.py
+++ wavbench/abc_benchmark.py
@@ -1,13 +1,7 @@
 import logging
-import time
-import wavbench.utils as utils
 from abc import ABC, abstractmethod
-from datetime import datetime, UTC
-from pathlib import Path
 from .providers.abc_provider import IaProvider
-from .transcribe import TranscribeResult, Measures
-from typing import Dict, List
-from .measures import get_measures, normalize_txt
+from typing import Dict
 
 logger: logging.Logger = logging.getLogger(__file__)
 
@@ -37,64 +31,3 @@ class BenchmarkABC(ABC):
             raise KeyError(f"Provider {name} does not exists.")
 
         self.providers.pop(name)
-
-    @staticmethod
-    def _run_provider(
-            provider: IaProvider,
-            audio: str,
-            reference: str,
-    ) -> TranscribeResult:
-        logger.debug(f"Run {provider.__class__.__name__} with audio: {audio}")
-
-        start: float = time.time()
-        hypothesis: str = provider.transcribe(audio)
-        runtime: float = utils.get_runtime(start)
-        duration: float = utils.get_audio_duration(audio)
-
-        measures: Measures = get_measures(reference, hypothesis)
-
-        return TranscribeResult(
-            audio=Path(audio).name,
-            provider_name=provider.name,
-            ia=provider.__class__.__name__,
-            params=provider.params,
-            hypothesis=normalize_txt(hypothesis),
-            reference=normalize_txt(reference),
-            measures=measures,
-            accuracy=round(((1 - measures.wer) * 100), 2),
-            runtime=runtime,
-            audio_duration=duration,
-            rtf=utils.get_rtf(runtime, duration)
-        )
-
-    @staticmethod
-    def _get_output_filename(name: str = "wavbench") -> str:
-        timestamp = datetime.now(UTC).strftime("%Y%m%dT%H%M%S")
-        return f"./results/{name}_{timestamp}"
-
-    @staticmethod
-    def _get_fieldnames() -> List[str]:
-        return [
-            "audio",
-            "ia",
-            "provider_name",
-            "params",
-            "reference",
-            "hypothesis",
-            "audio_duration",
-            "runtime",
-            "rtf",
-            "wer",
-            "wil",
-            "wip",
-            "cer",
-            "mer",
-            "accuracy"
-        ]
-
-    def _get_provider(self, name: str) -> IaProvider:
-        provider: IaProvider = self.providers.get(name)
-        if provider is None:
-            raise KeyError(f"Provider {name} does not exists.")
-
-        return provider
