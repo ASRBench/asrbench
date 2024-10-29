7ebaf7c2b8f5fe5040ca5b2f91d97a45b881a933
BDu4rTe <brianschierduarte@outlook.com>
2024-10-29 20:50:07 -0300

    feat(wavbench): impl observer in DefaultBenchmark and BenchmarkContext; rewrite DefaultBenchmark run flow

--- wavbench/benchmark.py
+++ wavbench/benchmark.py
@@ -1,97 +1,111 @@
-import csv
+import time
 import logging
+from . import jiwer_
+from . import utils
 from .abc_benchmark import BenchmarkABC
 from .benchmark_context import BenchmarkContext
 from .dataset import Dataset
-from .transcribe import TranscribeResult
+from .transcribe import TranscribeResult, TranscribePair, Measures
 from .providers.abc_provider import IaProvider
-from .output import OutputABC
+from .observer import Observer
+from .output_ctx import OutputContextABC
 from typing import List, Dict, Any
 
 logger: logging.Logger = logging.getLogger(__file__)
 
 
-class DatasetBenchmark(BenchmarkABC):
+class DefaultBenchmark(BenchmarkABC):
     def __init__(
             self,
             datasets: List[Dataset],
             providers: Dict[str, IaProvider],
-            output: OutputABC
+            output: OutputContextABC,
+            observer: Observer,
     ) -> None:
         self.__providers: Dict[str, IaProvider] = providers
         self.__datasets: List[Dataset] = datasets
-        self.__output: OutputABC = output
+        self.__output: OutputContextABC = output
+        self._observer: Observer = observer
+        self._context: BenchmarkContext = BenchmarkContext(datasets, observer)
 
     @property
     def providers(self) -> Dict[str, IaProvider]:
         return self.__providers
 
     def run(self) -> None:
-        fieldnames: List[str] = self._get_fieldnames()
-        fieldnames.append("dataset")
-
-        for dataset in self.__datasets:
-            logger.info(f"Run benchmark with dataset: {dataset.name}")
-            self.__output.filepath = self._get_output_filename(dataset.name)
-
-            with self.__output:
-                self._process_dataset_with_all_providers(
-                    BenchmarkContext(
-                        dataset=dataset,
-                        output=self.__output,
-                    )
-                )
+        with self.__output:
+            for idx, dataset in enumerate(self.__datasets):
+                logger.info(f"Run benchmark for dataset: {dataset.name}")
+                self._context.set_dataset(idx)
+                self._process_dataset_with_providers()
 
     def run_with_provider(self, name: str) -> None:
-        provider: IaProvider = self._get_provider(name)
-        fieldnames: List[str] = self._get_fieldnames()
-        fieldnames.append("dataset")
-
-        for dataset in self.__datasets:
-            logger.info(
-                f"Run wavbench with provider: {name} \
-                    for dataset: {dataset.name}",
-            )
-            output_filename: str = self._get_output_filename(
-                f"{dataset.name}_{provider.name}"
-            )
-
-            with open(output_filename, "w") as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                writer.writeheader()
-                self._process_dataset_pairs(
-                    BenchmarkContext(
-                        dataset=dataset,
-                        output=self.__output,
-                    ),
-                    provider,
+        with self.__output:
+            for idx, dataset in enumerate(self.__datasets):
+                logger.info(
+                    f"Run benchmark with provider: {name}"
+                    f"for dataset: {dataset.name}",
                 )
 
-    def _process_dataset_pairs(
-            self,
-            ctx: BenchmarkContext,
-            provider: IaProvider,
-    ) -> None:
-        ctx.start_progress()
-        for pair in ctx.dataset.pairs:
-            result: TranscribeResult = self._run_provider(
+                self._context.set_dataset(idx)
+                self._process_dataset_pairs(self._get_provider(name))
+
+    def _process_dataset_pairs(self, provider: IaProvider) -> None:
+        self._context.start_progress()
+        for pair in self._context.dataset.pairs:
+            result: TranscribeResult = self._run_transcribe(
                 provider,
-                pair.audio,
-                pair.reference,
+                pair,
             )
 
             final_result: Dict[str, Any] = result.to_dict()
-            final_result["dataset"] = ctx.dataset.name
 
-            ctx.update_progress(provider.name)
-            ctx.output.write_row(final_result)
+            self._context.update_progress(provider.name)
+            self.__output.write_row(final_result)
 
-    def _process_dataset_with_all_providers(
-            self,
-            ctx: BenchmarkContext
-    ) -> None:
+    def _process_dataset_with_providers(self) -> None:
         for provider_name, provider in self.providers.items():
             provider.load()
-            self._process_dataset_pairs(ctx, provider)
+            self._process_dataset_pairs(provider)
             provider.unload()
-            ctx.reset_progress()
+            self._context.reset_progress()
+
+    def _get_provider(self, name: str) -> IaProvider:
+        if name not in self.__providers:
+            raise KeyError(f"Provider {name} not in benchmark providers.")
+
+        return self.__providers[name]
+
+    def _run_transcribe(
+            self,
+            provider: IaProvider,
+            pair: TranscribePair,
+    ) -> TranscribeResult:
+        audio_path: str = pair.audio
+        reference: str = pair.reference
+
+        logger.debug(
+            f"Run {provider.__class__.__name__} with audio: {audio_path}",
+        )
+
+        start: float = time.time()
+        hypothesis: str = provider.transcribe(audio_path)
+        runtime: float = utils.get_runtime(start)
+        duration: float = utils.get_audio_duration(audio_path)
+
+        measures: Measures = jiwer_.get_measures(reference, hypothesis)
+
+        return TranscribeResult(
+            audio=utils.get_filename(audio_path),
+            provider_name=provider.name,
+            ia=provider.__class__.__name__,
+            params=provider.params,
+            hypothesis=jiwer_.normalize_txt(hypothesis),
+            reference=jiwer_.normalize_txt(reference),
+            measures=measures,
+            accuracy=round(((1 - measures.wer) * 100), 2),
+            runtime=runtime,
+            audio_duration=duration,
+            rtf=utils.get_rtf(runtime, duration),
+            dataset=self._context.dataset.name,
+        )
--- wavbench/benchmark_context.py
+++ wavbench/benchmark_context.py
@@ -1,57 +1,53 @@
-import sys
 import time
 from .dataset import Dataset
-from .output import OutputABC
-from .utils import get_runtime
+from .observer import Observer
+from typing import List
 
 
 class BenchmarkContext:
-    def __init__(
-            self,
-            dataset: Dataset,
-            output: OutputABC,
-    ) -> None:
-        self.__dataset: Dataset = dataset
-        self.__output: OutputABC = output
-        self.__total_pairs: int = len(self.__dataset.pairs)
-        self._processed_pairs: int = 0
-        self.__start: float = 0.0
-
-    @property
-    def output(self) -> OutputABC:
-        return self.__output
+    def __init__(self, datasets: List[Dataset], observer: Observer) -> None:
+        self.__datasets: List[Dataset] = datasets
+        self._total: int = 0
+        self._current: int = 0
+        self._start: float = 0.0
+        self._progress: float = 0.0
+        self._observer: Observer = observer
+        self.__dataset = None
 
     @property
     def dataset(self) -> Dataset:
+        if self.__dataset is None:
+            raise ValueError(
+                "Context dataset is none, verify benchmark run methods.",
+            )
+
         return self.__dataset
 
+    def set_dataset(self, idx: int) -> None:
+        self.__dataset: Dataset = self.__datasets[idx]
+        self._total = len(self.__dataset.pairs)
+
     def start_progress(self) -> None:
-        self.__start = time.time()
+        self._start: float = time.time()
 
     def reset_progress(self) -> None:
-        self._processed_pairs = 0
-        self.__start = time.time()
+        self._current: int = 0
+        self._progress: float = 0.0
 
     def update_progress(self, provider_name: str) -> None:
-        self._processed_pairs += 1
-        self._display_message(self._get_progress_msg(provider_name))
+        self._current += 1
+        self._observer.update_progress(
+            self._get_progress(),
+            self._get_progress_msg(provider_name),
+        )
 
-        if self._processed_pairs == self.__total_pairs:
-            self._display_message(self._get_finish_msg())
+        if self._current == self._total:
+            self._observer.finish()
+
+    def _get_progress(self) -> float:
+        self._progress = self._current / self._total
+        return self._progress
 
     def _get_progress_msg(self, provider: str) -> str:
         dataset: str = self.__dataset.name
-        percent: float = round(
-            (self._processed_pairs / self.__total_pairs) * 100,
-            2
-        )
-        return f"\r[{percent}%] Processing pairs from {dataset} with {provider}"
-
-    def _get_finish_msg(self) -> str:
-        runtime = get_runtime(self.__start) / 60
-        return f"\tFinish process in {runtime:.2f} minutes.\n"
-
-    @staticmethod
-    def _display_message(msg: str) -> None:
-        sys.stdout.write(msg)
-        sys.stdout.flush()
+        return f"Processing pairs from {dataset} with {provider}..."
